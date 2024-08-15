ef12613f9a6e46e435ea09f4adb533a2d9b46f23
Mert Topçu <merttopcu95@gmail.com>
2024-08-15 13:23:42 +0300

    PatikaSqlBasicsTask3.sql

--- PatikaSqlBasicsTask3.sql
+++ PatikaSqlBasicsTask3.sql
@@ -1,19 +1,19 @@
--- 1. country tablosunda bulunan country sütunundaki ülke isimlerinden 'A' karakteri ile başlayıp 'a' karakteri ile sonlananları sıralayınız.
+-- 1. Select all columns from the country table where the country column starts with 'A' and ends with 'a'.
 SELECT *
 FROM country
 WHERE country LIKE 'A%a';
 
--- 2. country tablosunda bulunan country sütunundaki ülke isimlerinden en az 6 karakterden oluşan ve sonu 'n' karakteri ile sonlananları sıralayınız.
+-- 2. Select all columns from the country table where the country column has at least 6 characters and ends with 'n'.
 SELECT *
 FROM country
 WHERE country LIKE '_____%n';
 
--- 3. film tablosunda bulunan title sütunundaki film isimlerinden en az 4 adet büyük ya da küçük harf farketmesizin 'T' karakteri içeren film isimlerini sıralayınız.
+-- 3. Select all columns from the film table where the title column contains at least one 'T', case-insensitive.
 SELECT *
 FROM film
 WHERE title ~~* '%T%';
 
--- 4. film tablosunda bulunan tüm sütunlardaki verilerden title 'C' karakteri ile başlayan ve uzunluğu (length) 90 dan büyük olan ve rental_rate 2.99 olan verileri sıralayınız.
+-- 4. Select all columns from the film table where title starts with 'C', length is greater than 90, and rental_rate is 2.99.
 SELECT *
 FROM film
 WHERE title LIKE 'C%' AND length>90 AND rental_rate=2.99;
